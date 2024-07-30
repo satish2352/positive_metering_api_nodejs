4c751832e4ee357399de46e2af0d9b3880736400
shubham <shubham.kothavade09@gmail.com>
2024-07-30 11:38:05 +0530

    changes

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(cors(corsOptions));
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
 });
