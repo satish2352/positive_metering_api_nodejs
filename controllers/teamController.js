8eafa23d1d756fe7e5fb40a8b901b54fb728d498
shubham <shubham.kothavade09@gmail.com>
2024-08-23 12:52:54 +0530

    changes

--- controllers/teamController.js
+++ controllers/teamController.js
@@ -5,77 +5,28 @@ const apiResponse = require('../helper/apiResponse');
 const { validationResult } = require('express-validator');
 
 // Add a new team member
-// exports.addTeamMember = async (req, res) => {
-//   const errors = validationResult(req);
-//   if (!errors.isEmpty()) {
-//     return apiResponse.ErrorResponse(res, errors.array().map(err => err.msg).join(', '));
-//   }
-
-//   const transaction = await sequelize.transaction();
-  
-//   try {
-//     const { name, designation, description, position_no } = req.body;
-//     const img = req.file ? req.file.path : null;
-
-//     // Increment positions of existing members
-//     await Team.update(
-//       { position_no: Sequelize.literal('position_no + 1') },
-//       { 
-//         where: { position_no: { [Sequelize.Op.gte]: position_no }, isDelete: false },
-//         transaction
-//       }
-//     );
-
-//     // Create the new team member
-//     const teamMember = await Team.create({
-//       img,
-//       name,
-//       designation,
-//       description,
-//       position_no,
-//       isActive: true,
-//       isDelete: false,
-//     }, { transaction });
-
-//     await transaction.commit();
-
-//     return apiResponse.successResponseWithData(
-//       res,
-//       'Team member added successfully',
-//       teamMember
-//     );
-//   } catch (error) {
-//     await transaction.rollback();
-//     console.error('Add team member failed', error);
-//     return apiResponse.ErrorResponse(res, 'Add team member failed');
-//   }
-// };
-
-// Add a new team member
 exports.addTeamMember = async (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return apiResponse.ErrorResponse(res, errors.array().map(err => err.msg).join(', '));
   }
 
-  const transaction = await sequelize.transaction({
-    isolationLevel: Sequelize.Transaction.ISOLATION_LEVELS.SERIALIZABLE,
-  });
-
+  const transaction = await sequelize.transaction();
+  
   try {
     const { name, designation, description, position_no } = req.body;
     const img = req.file ? req.file.path : null;
 
-    // Step 1: Temporarily increment all positions by 2 to create space
+    // Increment positions of existing members
     await Team.update(
-      { position_no: Sequelize.literal('position_no + 2') },
+      { position_no: Sequelize.literal('position_no + 1') },
       { 
         where: { position_no: { [Sequelize.Op.gte]: position_no }, isDelete: false },
-        transaction,
+        transaction
       }
     );
 
-    // Step 2: Insert the new team member at the specified position
+    // Create the new team member
     const teamMember = await Team.create({
       img,
       name,
@@ -85,18 +36,6 @@ exports.addTeamMember = async (req, res) => {
       isActive: true,
       isDelete: false,
     }, { transaction });
-
-    // Step 3: Normalize the positions back to consecutive numbers
-    const teamMembers = await Team.findAll({
-      where: { isDelete: false },
-      order: [['position_no', 'ASC']],
-      transaction,
-    });
-
-    for (let i = 0; i < teamMembers.length; i++) {
-      teamMembers[i].position_no = i + 1; // Assign new consecutive position numbers
-      await teamMembers[i].save({ transaction });
-    }
 
     await transaction.commit();
 
@@ -111,6 +50,7 @@ exports.addTeamMember = async (req, res) => {
     return apiResponse.ErrorResponse(res, 'Add team member failed');
   }
 };
+
 
 
 // Update an existing team member
