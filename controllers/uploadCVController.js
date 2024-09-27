d0638cfa113672afb2f7fb87d702f4416946a2b8
shubham <shubham.kothavade09@gmail.com>
2024-09-27 17:57:24 +0530

    cv changes

--- controllers/uploadCVController.js
+++ controllers/uploadCVController.js
@@ -1,49 +1,62 @@
-const UploadCV = require('../models/UploadCV');
-const apiResponse = require('../helper/apiResponse');
-const sendEmail = require('../middleware/nodemailer');
+const UploadCV = require("../models/UploadCV");
+const apiResponse = require("../helper/apiResponse");
+const sendEmail = require("../middleware/nodemailer");
 
 exports.addUploadCV = async (req, res) => {
   try {
     const { name, email, phone, subject, message } = req.body;
-    const cv = req.file ? req.file.path : null;
-
-    const uploadCV = await UploadCV.create({ name, email, phone, subject, message, cv, isActive: true, isDelete: false });
+    let newDataVal = req.file ? req.file.path : null;
+    const cv = newDataVal.replace(/\\/g, "/");
+    const uploadCV = await UploadCV.create({
+      name,
+      email,
+      phone,
+      subject,
+      message,
+      cv,
+      isActive: true,
+      isDelete: false,
+    });
 
     // Set email options for the sendEmail middleware
     req.emailOptions = {
       to: process.env.EMAIL_SENT_TO_HR,
-      subject: 'New Job Application',
-      text: `A new CV has been uploaded:\n\nName: ${name}\nEmail: ${email}\nPhone: ${phone}\nSubject: ${subject}\nMessage: ${message}\nCV: [Download CV](${req.protocol}://${req.get('host')}/${cv})`,
+      subject: "New Job Application",
+      text: `A new CV has been uploaded:\n\nName: ${name}\nEmail: ${email}\nPhone: ${phone}\nSubject: ${subject}\nMessage: ${message}\nDownload CV: ${process.env.SERVER_PATH}${cv}`,
     };
+    console.log("cv__________", cv);
 
     // Send the email
     await sendEmail(req, res, (err) => {
       if (err) {
-        return apiResponse.ErrorResponse(res, 'Failed to send email');
+        return apiResponse.ErrorResponse(res, "Failed to send email");
       }
 
       // If the email is sent successfully, return the success response
-      return apiResponse.successResponseWithData(res, 'CV uploaded successfully', uploadCV);
+      return apiResponse.successResponseWithData(
+        res,
+        "CV uploaded successfully",
+        uploadCV
+      );
     });
-
   } catch (error) {
-    console.error('Upload CV failed', error);
+    console.error("Upload CV failed", error);
 
-    if (error.name === 'SequelizeUniqueConstraintError') {
+    if (error.name === "SequelizeUniqueConstraintError") {
       const fields = error.errors.map((err) => err.path);
-      let message = 'Validation error: ';
+      let message = "Validation error: ";
 
-      if (fields.includes('email')) {
-        message += 'Email already exists. ';
+      if (fields.includes("email")) {
+        message += "Email already exists. ";
       }
-      if (fields.includes('phone')) {
-        message += 'Phone number already exists. ';
+      if (fields.includes("phone")) {
+        message += "Phone number already exists. ";
       }
 
       return apiResponse.validationErrorWithData(res, message.trim());
     }
 
-    return apiResponse.ErrorResponse(res, 'Upload CV failed');
+    return apiResponse.ErrorResponse(res, "Upload CV failed");
   }
 };
 
@@ -55,7 +68,7 @@ exports.updateUploadCV = async (req, res) => {
 
     const uploadCV = await UploadCV.findByPk(id);
     if (!uploadCV) {
-      return apiResponse.notFoundResponse(res, 'CV not found');
+      return apiResponse.notFoundResponse(res, "CV not found");
     }
 
     uploadCV.name = name;
@@ -66,24 +79,28 @@ exports.updateUploadCV = async (req, res) => {
     uploadCV.cv = cv || uploadCV.cv;
     await uploadCV.save();
 
-    return apiResponse.successResponseWithData(res, 'CV updated successfully', uploadCV);
+    return apiResponse.successResponseWithData(
+      res,
+      "CV updated successfully",
+      uploadCV
+    );
   } catch (error) {
-    if (error.name === 'SequelizeUniqueConstraintError') {
+    if (error.name === "SequelizeUniqueConstraintError") {
       const fields = error.errors.map((err) => err.path);
-      let message = 'Validation error: ';
-      
-      if (fields.includes('email')) {
-        message += 'Email already exists. ';
+      let message = "Validation error: ";
+
+      if (fields.includes("email")) {
+        message += "Email already exists. ";
       }
-      if (fields.includes('phone')) {
-        message += 'Phone number already exists. ';
+      if (fields.includes("phone")) {
+        message += "Phone number already exists. ";
       }
 
       return apiResponse.validationErrorWithData(res, message.trim());
     }
 
-    console.error('Update CV failed', error);
-    return apiResponse.ErrorResponse(res, 'Update CV failed');
+    console.error("Update CV failed", error);
+    return apiResponse.ErrorResponse(res, "Update CV failed");
   }
 };
 
@@ -92,17 +109,21 @@ exports.getUploadCVs = async (req, res) => {
     const uploadCVs = await UploadCV.findAll({ where: { isDelete: false } });
 
     const baseUrl = `${process.env.SERVER_PATH}`;
-        const uploadCVsWithBaseUrl = uploadCVs.map(cv => {
+    const uploadCVsWithBaseUrl = uploadCVs.map((cv) => {
       return {
         ...cv.toJSON(),
-        cv: cv.cv ? baseUrl + cv.cv.replace(/\\/g, '/') : null
+        cv: cv.cv ? baseUrl + cv.cv.replace(/\\/g, "/") : null,
       };
     });
 
-    return apiResponse.successResponseWithData(res, 'CVs retrieved successfully', uploadCVsWithBaseUrl);
+    return apiResponse.successResponseWithData(
+      res,
+      "CVs retrieved successfully",
+      uploadCVsWithBaseUrl
+    );
   } catch (error) {
-    console.error('Get CVs failed', error);
-    return apiResponse.ErrorResponse(res, 'Get CVs failed');
+    console.error("Get CVs failed", error);
+    return apiResponse.ErrorResponse(res, "Get CVs failed");
   }
 };
 
@@ -112,16 +133,20 @@ exports.toggleCVStatus = async (req, res) => {
     const uploadCV = await UploadCV.findByPk(id);
 
     if (!uploadCV) {
-      return apiResponse.notFoundResponse(res, 'CV not found');
+      return apiResponse.notFoundResponse(res, "CV not found");
     }
 
     uploadCV.isActive = !uploadCV.isActive;
     await uploadCV.save();
 
-    return apiResponse.successResponseWithData(res, 'CV status updated successfully', uploadCV);
+    return apiResponse.successResponseWithData(
+      res,
+      "CV status updated successfully",
+      uploadCV
+    );
   } catch (error) {
-    console.error('Toggle CV status failed', error);
-    return apiResponse.ErrorResponse(res, 'Toggle CV status failed');
+    console.error("Toggle CV status failed", error);
+    return apiResponse.ErrorResponse(res, "Toggle CV status failed");
   }
 };
 
@@ -131,15 +156,19 @@ exports.toggleCVDelete = async (req, res) => {
     const uploadCV = await UploadCV.findByPk(id);
 
     if (!uploadCV) {
-      return apiResponse.notFoundResponse(res, 'CV not found');
+      return apiResponse.notFoundResponse(res, "CV not found");
     }
 
     uploadCV.isDelete = !uploadCV.isDelete;
     await uploadCV.save();
 
-    return apiResponse.successResponseWithData(res, 'CV delete status updated successfully', uploadCV);
+    return apiResponse.successResponseWithData(
+      res,
+      "CV delete status updated successfully",
+      uploadCV
+    );
   } catch (error) {
-    console.error('Toggle CV delete status failed', error);
-    return apiResponse.ErrorResponse(res, 'Toggle CV delete status failed');
+    console.error("Toggle CV delete status failed", error);
+    return apiResponse.ErrorResponse(res, "Toggle CV delete status failed");
   }
 };
--- middleware/multerCV.js
+++ middleware/multerCV.js
@@ -1,27 +1,31 @@
-const multer = require('multer');
-const path = require('path');
-const fs = require('fs');
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
 
 // Define storage for multer
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    const uploadFolder = 'uploads/cvs';
-    const uploadPath = path.join(__dirname, '..', uploadFolder);
+    const uploadFolder = "uploads/cvs";
+    const uploadPath = path.join(__dirname, "..", uploadFolder);
     if (!fs.existsSync(uploadPath)) {
       fs.mkdirSync(uploadPath, { recursive: true });
     }
     cb(null, uploadFolder);
   },
+ 
   filename: (req, file, cb) => {
-    cb(null, `${Date.now()}-${file.originalname}`);
-  }
+    let data = file.originalname;
+    let updatedData = data.replace(/\s+/g, "-");
+    console.log("file.originalname", updatedData);
+    cb(null, `${Date.now()}-${updatedData}`);
+  },
 });
 
 // File filter to only allow certain file types
 const fileFilter = (req, file, cb) => {
   const ext = path.extname(file.originalname);
-  if (ext !== '.pdf' && ext !== '.doc' && ext !== '.docx') {
-    return cb(new Error('Only PDF and DOC files are allowed'), false);
+  if (ext !== ".pdf" && ext !== ".doc" && ext !== ".docx") {
+    return cb(new Error("Only PDF and DOC files are allowed"), false);
   }
   cb(null, true);
 };
@@ -35,8 +39,11 @@ const upload = multer({
 
 // Custom middleware to check file size after upload
 const validateFileSize = (req, res, next) => {
-  if (req.file && req.file.size > 1024 * 1024) { // 300 KB
-    return res.status(400).json({ error: 'File size must be less than 300 KB' });
+  if (req.file && req.file.size > 1024 * 1024) {
+    // 300 KB
+    return res
+      .status(400)
+      .json({ error: "File size must be less than 1024 KB" });
   }
   next();
 };
