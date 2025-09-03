53eecc7e5dfd84cbd3e561fbec670f34c5608a57
Sumago-Git <shubhamkothavade@sumagoinfotech.in>
2025-09-03 17:19:19 +0530

    changes

--- controllers/blogDetailController.js
+++ controllers/blogDetailController.js
@@ -245,7 +245,6 @@ exports.getBlogPage = async (req, res) => {
   try {
     const { slug } = req.params;
     const userAgent = req.headers["user-agent"] || "";
-    console.log("userAgentuserAgentuserAgentuserAgent", req);
     const blog = await BlogDetail.findOne({ where: { slug } });
     if (!blog) {
       // Always return 200 to bots to avoid scraping errors
